34e0602bfd5fc4bd9c724f32d6e69b89f0f05f44
glaubert <suyann12@hotmail.com>
2023-01-27 18:29:53 -0300

    :sparkles: updated default route

--- server/app/main.py
+++ server/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! MyFriend'
+    
+    my_keys = {
+        'DB_HOST',
+        'DB_USER',
+        'DB_PASS'
+    }
+
+    if(my_keys <= os.environ.keys()):
+
+        return f""" DB_HOST: {os.environ['DB_HOST']}<br />
+                    DB_USER: {os.environ['DB_USER']}<br />
+                    DB_PASS: {os.environ['DB_PASS']}<br />
+                    DB_DB: {os.environ['DB_BASE']}<br />
+                    DB_PORT: {os.environ['DB_PORT']}<br />
+                    """
+
+    return "Hello, World!<br/>Here didn't have a special keys<br/>Try again later"
 
 if __name__ == '__main__':
     app.run(debug=True)
