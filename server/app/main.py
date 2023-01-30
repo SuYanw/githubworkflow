b2ceb3ac65906ad3e40b771969f7c0f64adebf39
glaubert <suyann12@hotmail.com>
2023-01-30 10:04:26 -0300

    :bug: updated test file

--- server/app/main.py
+++ server/app/main.py
@@ -8,24 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     
-    my_keys = {
-        'DB_HOST',
-        'DB_USER',
-        'DB_PASS'
-    }
-
-    if(my_keys <= os.environ.keys()):
-
-        return f""" DB_HOST: {os.environ['DB_HOST']}<br />
-                    DB_USER: {os.environ['DB_USER']}<br />
-                    DB_PASS: {os.environ['DB_PASS']}<br />
-                    DB_DB: {os.environ['DB_BASE']}<br />
-                    DB_PORT: {os.environ['DB_PORT']}<br />
-                    """
+    out_str = []
     for kvl  in os.environ:
-        print(f"{kvl}={os.environ.get(kvl)}")
-
-    return "Hello, World!<br/>Here didn't have a special keys<br/>Try again later"
+        out_str.append(f"{kvl}={os.environ.get(kvl)} <br/>")
+        
+    return out_str
 
 if __name__ == '__main__':
     app.run(debug=True)
